76d373854f3054fd219ef3fe74589d240b236963
Valentino <collazo.valentino@gmail.com>
2021-11-23 14:47:42 -0300

    Add energy Tileset

--- src/Managers/BoardManager/Grid/EnergyGrid.gd
+++ src/Managers/BoardManager/Grid/EnergyGrid.gd
@@ -1,6 +1,7 @@
 extends "res://src/Managers/BoardManager/Grid/Grid.gd"
 
 var edited_cells = []
+onready var level_grid = get_parent().get_node("LevelGrid")
 
 # These fuctions are used to edit each individual cells
 func add_energy(map_position: Vector2, amount: int) -> void:
@@ -11,6 +12,19 @@ func set_energy(map_position: Vector2, amount: int) -> void:
 	if get_cell(map_position) < amount:
 		set_cell(map_position, amount)
 	edited_cells.append(map_position)
+	match amount:
+		0:
+			level_grid.set_cell(map_position.x, map_position.y, 0)
+		1:
+			level_grid.set_cell(map_position.x, map_position.y, 1)
+		2:
+			level_grid.set_cell(map_position.x, map_position.y, 2)
+		3:
+			level_grid.set_cell(map_position.x, map_position.y, 3)
+		4:
+			level_grid.set_cell(map_position.x, map_position.y, 4)
+		5:
+			level_grid.set_cell(map_position.x, map_position.y, 5)
 
 # Function is called at the end of turn. Should automatically update energy stuff
 func drain_energy() -> void:
