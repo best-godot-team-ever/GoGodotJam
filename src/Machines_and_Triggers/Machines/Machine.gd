582e1451a482a2eda1ecb56273759017d80bdb4b
Valentino <collazo.valentino@gmail.com>
2021-11-24 00:05:29 -0300

    machines FIXED

--- src/Machines_and_Triggers/Machines/Machine.gd
+++ src/Machines_and_Triggers/Machines/Machine.gd
@@ -8,7 +8,7 @@ var trigger_feeding_list : Array = []
 func _ready() -> void:
 	for trigger_path in _link_with_triggers:
 		var trigger = get_node(trigger_path)
-		trigger.machine_conection_list.append(trigger)
+		trigger.machine_conection_list.append(self)
 
 func start_turn() -> void:
 	if ! trigger_feeding_list.empty():
