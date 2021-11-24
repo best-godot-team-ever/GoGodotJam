69eb49939623c7cdb0b525fbca4a238abee8d1ce
Valentino <collazo.valentino@gmail.com>
2021-11-24 17:14:41 -0300

    finished 4th mini level

--- src/Machines_and_Triggers/Machines/DoorChanging.gd
+++ src/Machines_and_Triggers/Machines/DoorChanging.gd
@@ -3,19 +3,19 @@ extends Machine
 var _activated := false 
 
 func _initialize(board_manager: Node, turn_manager: Node) -> void:
-	machine_type = MachineType.Door
+	machine_type = MachineType.DoorChanging
 	._initialize(board_manager, turn_manager)
 
 func start_turn() -> void:
 	if ! trigger_feeding_list.empty():
 		if _activated == false:
-#			_activated = true
+			_activated = true
 			_board_manager.set_machine_blocking(machine_id, false)
 			_animated_sprite.play("activated")
-#		if _activated == true:
-#			_activated = false
-#			_board_manager.set_machine_blocking(machine_id, true)
-#			_animated_sprite.play("deactivated")
+		elif _activated == true:
+			_activated = false
+			_board_manager.set_machine_blocking(machine_id, true)
+			_animated_sprite.play("deactivated")
 	if trigger_feeding_list.empty():
 			pass
 
