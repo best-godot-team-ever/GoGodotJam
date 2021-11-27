187d0c5836184cfe931e38ec4431202829d80d39
Valentino <collazo.valentino@gmail.com>
2021-11-27 19:31:45 -0300

    added level transition, added sprite and sfx folders

--- src/Machines_and_Triggers/Machines/Door.gd
+++ src/Machines_and_Triggers/Machines/Door.gd
@@ -11,5 +11,7 @@ func start_turn() -> void:
 			_animated_sprite.play("activated")
 	if trigger_feeding_list.empty():
 			_board_manager.set_machine_blocking(machine_id, true)
-			_animated_sprite.play("deactivated")
-
+			if _player.get_current_position() != _board_manager.world_to_map(position):
+				_animated_sprite.play("deactivated")
+#
+#if _board_manager.get_cell(_board_manager.world_to_map(position)).is_blocked and ! _used:
--- src/Machines_and_Triggers/Machines/DoorReverse.gd
+++ src/Machines_and_Triggers/Machines/DoorReverse.gd
@@ -8,7 +8,8 @@ func _initialize(board_manager: Node, turn_manager: Node) -> void:
 func start_turn() -> void:
 	if ! trigger_feeding_list.empty():
 		_board_manager.set_machine_blocking(machine_id, true)
-		_animated_sprite.play("deactivated")
+		if _player.get_current_position() != _board_manager.world_to_map(position):
+			_animated_sprite.play("deactivated")
 	if trigger_feeding_list.empty():
 		_board_manager.set_machine_blocking(machine_id, false)
 		_animated_sprite.play("activated")
